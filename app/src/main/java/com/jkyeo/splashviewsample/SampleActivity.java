2ce380be4a6f45bda6d30d5512ffc3737182d3f6
jkyeo <ykooze@gmail.com>
2016-09-11 15:50:01 +0800

    test build

--- app/src/main/java/com/jkyeo/splashviewsample/SampleActivity.java
+++ app/src/main/java/com/jkyeo/splashviewsample/SampleActivity.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 import com.jkyeo.splashview.SplashView;
 
 public class SampleActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
